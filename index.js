5e79d2ca74c587d6610a57f6057ad132d7bde6a1
Mary Han <maryzhenghan@gmail.com>
2018-03-31 18:06:28 -0400

    Build zipcode & city/country code forms on weather page
    Forms built in 'index.html'. Basic CSS styling was added in 'index.css'.
    Also started working on a function that tracks the submitted query, and added a constant to start working on OpenWeatherMap API with.

--- index.js
+++ index.js
@@ -4,10 +4,24 @@ function startApp() {
 
 		$('.js-homepage').css('display','none');
 		$('.js-weatherpage').css('display','inline-block');
+
+		submitLocation();
 	});
 }
 
+const OPENWEATHER_SEARCH_URL = 'http://api.openweathermap.org/data/2.5/weather';
 
+function submitLocation() {
+	$('.js-form-zipcode').submit(function(event) {
+		const zipcodeTarget = $(event.currentTarget).find('.js-zipcode');
+		const zipcode = zipcodeTarget.val();
+		console.log(zipcode);
+		event.preventDefault();
+	});
+
+
+	
+}
 
 
 function callBack() {
